5983a9b422367cbde3225066e0291ea9db4f9162
HEEJIN <heehee2310@gmail.com>
2021-03-28 02:00:27 +0900

    Remove unusing method

--- src/implementation/boj1756_피자굽기.java
+++ src/implementation/boj1756_피자굽기.java
@@ -1,33 +1,38 @@
 package implementation;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class boj1756_피자굽기 {
 
     public static int stoi(String s) {return Integer.parseInt(s);}
 
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int D = sc.nextInt();
-        int N = sc.nextInt();
+        int D = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
+        st = new StringTokenizer(br.readLine());
         int[] oven = new int[D+1];
 
         int max = 1_000_000_000;
         for (int i = 1; i <= D; i++) {
-            oven[i] = sc.nextInt();
+            oven[i] = stoi(st.nextToken());
             if (oven[i] > max) oven[i] = max;
             else max = oven[i];
         }
 
         int result = D+1;
 
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            int pizza = sc.nextInt();
-
+            int pizza = stoi(st.nextToken());
             if (result == 0) continue;
+
             for (int d = result-1; d >= 0; d--) {
                 if (pizza <= oven[d]) {
                     result = d;
